d67f7e59ea6ddaf78859043d436319bf29bf0196
yyeryy <yerayaller@gmail.com>
2025-09-18 18:27:14 +0200

    img

--- src/components/jornadas.tsx
+++ src/components/jornadas.tsx
@@ -119,6 +119,7 @@ export const JornadasPanel = () => {
         <div className="tableWrapper">
           {jornada.every((j) => j.puntos === 0) ? (
             <div className="spinnerWrapper">
+              <img src={"../imagenes/spinner.jpg"} className="rotatingImage" />
               <p>Jornada aún no jugada</p>
             </div>
           ) : (
